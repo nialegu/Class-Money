33cf79dd2d988f6e4916b669b45e02e55bb72bd8
Leonid <rastrast207@gmail.com>
2023-09-22 17:07:46 +0300

    Add overloaded arithmetic operations; fix processing negative values to output on the screen

--- Money/main.cpp
+++ Money/main.cpp
@@ -15,9 +15,9 @@ private:
 	long double value;
 public:
 	money() : value(0) {}
-	money(long double m) : value(m) {}
+	explicit money(long double m) : value(m) {} 
 
-	long double mstold(char []); //money string to long double
+	long double mstold(char[]); //money string to long double
 	void ldtoms(long double); //long double to money string
 
 	void get_money() {
@@ -29,19 +29,53 @@ public:
 		ldtoms(value);
 	}
 
-	/*money operator+(money);
-	money operator-(money);
-	money operator*(money);
-	money operator/(long double);
-	long double operator/(money);*/
+	money operator+(money) const;
+	money operator-(money) const;
+	money operator*(long double) const;
+	money operator/(long double) const;
+	long double operator/(money) const;
 };
 
 int main() {
-	money a, b;
-	a.get_money();
-	a.put_money();
+	money a, b, c;
+	long double time_oneitem, item_counter;
+	cout << "1: "; a.get_money();
+	cout << "2: "; b.get_money();
 
+	c = a + b; cout << "1st$ + 2nd$ = "; c.put_money();
+	c = a - b; cout << "1st$ - 2nd$ = "; c.put_money();
+	cout << "Time for one item: "; cin >> time_oneitem;
+	c = a * time_oneitem; cout << "Total price for one item per entered time: "; c.put_money();
+	cout << "Count of products: "; cin >> item_counter;
+	c = a / item_counter; cout << "Price for one item: "; c.put_money();
+	item_counter = a / c; cout << "Count of products: " << item_counter;
+	
 	return 0;
+}
+
+money money::operator+(money m2) const {
+	long double m = value + m2.value;
+	return money(m);
+}
+
+money money::operator-(money m2) const {
+	long double m = value - m2.value;
+	return money(m);
+}
+
+money money::operator*(long double t) const {
+	long double m = value * t;
+	return money(m);
+}
+
+money money::operator/(long double c) const {
+	long double m = value / c;
+	return money(m);
+}
+
+long double money::operator/(money m2) const {
+	long double c = value / m2.value;
+	return c;
 }
 
 void money::ldtoms(long double m) {
@@ -49,49 +83,59 @@ void money::ldtoms(long double m) {
 
 	ostringstream mstr;
 	mstr << fixed << setprecision(2) << m;
-	
+
 	char ms[27] = "$";
-	int count, j=0, c=0;
+	int cnt, j = 0, c = 0;
 
 	for (int i = 0; i < 19; i++) {
 		if (mstr.str().c_str()[i] != '.') c++;
 		else break;
 	}
-	count = 3 - c % 3;
-	if (count == 3) count = 0;
-
+	cnt = 3 - c % 3;
+	if (cnt == 3) cnt = 0;
+	//cout << count << endl;
 	ms[26] = '\0'; // don't forget add \0 element to the character's array end - this is very important!
 	for (int i = 0; i < 18; i++) {
-		if (count != 3) {
-			ms[i + 1 + j] = mstr.str().c_str()[i];
-			count++;
+		if (mstr.str().c_str()[i] == '-') {
+			ms[i + 1] = mstr.str().c_str()[i];
+			cnt++;
+			if (cnt == 3) cnt = 0;
 		}
-		else if (count == 3 && mstr.str().c_str()[i] != '.') {
+		else if (cnt != 3) {
+			ms[i + 1 + j] = mstr.str().c_str()[i];
+			cnt++;
+		}
+		else if (cnt == 3 && mstr.str().c_str()[i] != '.') {
 			ms[i + 1 + j] = ',';
 			j++;
 			ms[i + 1 + j] = mstr.str().c_str()[i];
-			count = 1;
+			cnt = 1;
 		}
-		else if (count == 3 && mstr.str().c_str()[i] == '.') {
-			count = 0;
+		else if (cnt == 3 && mstr.str().c_str()[i] == '.') {
+			cnt = 0;
 			ms[i + 1 + j] = mstr.str().c_str()[i];
 		}
 	}
+	if (ms[1] == '-') {
+		char temp;
+		temp = ms[0]; ms[0] = ms[1]; ms[1] = temp;
+	} // fixing negative number's format for writing 
+
 	cout << ms << endl;
 }
 
 long double money::mstold(char m[]) {
 	char mvd[19];
-	int j=0;
+	int j = 0;
 	for (int i = 0; i < 18; i++) {
 		if (m[i] == '0' || m[i] == '1' || m[i] == '2' || m[i] == '3' || m[i] == '4' || m[i] == '5' ||
 			m[i] == '6' || m[i] == '7' || m[i] == '8' || m[i] == '9' || m[i] == '.') {
-			
+
 			mvd[j++] = m[i];
 		}
 	}
 	mvd[18] = '\0'; //add \0 element to the character's array end is a really important!!!
-	
+
 	value = static_cast<long double>(atof(mvd));
 	return value;
 }
