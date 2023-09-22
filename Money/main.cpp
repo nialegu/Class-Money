484796196556d82645fc02adc11b10b3f87cf293
Leonid <rastrast207@gmail.com>
2023-09-22 23:47:15 +0300

    Add Sterling class similar to Dollars class; rename Money class to Dollars

--- Money/main.cpp
+++ Money/main.cpp
@@ -7,78 +7,161 @@
 #include <string>
 #include <windows.h>
 #include <sstream>
+#include <math.h>
 
 using namespace std;
 
-class money {
+class dollars {
 private:
 	long double value;
 public:
-	money() : value(0) {}
-	explicit money(long double m) : value(m) {} 
+	dollars() : value(0) {}
+	explicit dollars(long double m) : value(m) {} 
 
 	long double mstold(char[]); //money string to long double
 	void ldtoms(long double); //long double to money string
 
-	void get_money() {
+	void get_dollars() {
 		char str[19];
 		cout << "Enter a count of money: "; cin >> str;
 		mstold(str);
 	}
-	void put_money() {
+	void put_dollars() {
 		ldtoms(value);
 	}
 
-	money operator+(money) const;
-	money operator-(money) const;
-	money operator*(long double) const;
-	money operator/(long double) const;
-	long double operator/(money) const;
+	dollars operator+(dollars) const;
+	dollars operator-(dollars) const;
+	dollars operator*(long double) const;
+	dollars operator/(long double) const;
+	long double operator/(dollars) const;
+};
+
+class sterling {
+private:
+	long pounds;
+	int shillings;
+	int pence;
+public:
+	sterling() : pounds(0), shillings(0), pence(0) {}
+	sterling(double p) {
+		double temp = abs(p - static_cast<int>(p));
+		pounds = static_cast<long>(p - temp);
+		shillings = static_cast<int>(temp * 20);
+		pence = static_cast<int>(temp * 240 - shillings*12);
+	}
+	sterling(long p, int s, int pn) : pounds(p), shillings(s), pence(pn) {}
+
+	void get_sterling() {
+		char dummy_point;
+		cout << "Enter a count of sterling (format J*.*.*): J";
+		cin >> pounds >> dummy_point >> shillings >> dummy_point >> pence;
+		
+		if (pounds < 0 || shillings < 0 || pence < 0) {
+			cout << "\nIncorrect! Values must to be positive. \nTry again.\n\n";
+			get_sterling();
+		}
+		if (shillings >= 20 || pence >= 12) {
+			cout << "\nIncorrect! Shillings can't to be more or equal to 20; pences can't to be more or equal to 12.\n"
+				<< "Try again.\n\n";
+			get_sterling();
+		}
+	}
+
+	void put_sterling() const {
+		cout << "J" << pounds << "." << shillings << "." << pence << endl;
+	}
+
+	sterling operator+(sterling) const;
+	sterling operator-(sterling) const;
+	sterling operator*(double) const;
+	sterling operator/(double) const;
+	double operator/(sterling) const;
+	operator double();
+	operator dollars();
 };
 
 int main() {
-	money a, b, c;
-	long double time_oneitem, item_counter;
-	cout << "1: "; a.get_money();
-	cout << "2: "; b.get_money();
+	sterling s1;
+	s1.get_sterling();
+	s1.put_sterling();
+	dollars d1 = s1;
+	d1.put_dollars();
 
-	c = a + b; cout << "1st$ + 2nd$ = "; c.put_money();
-	c = a - b; cout << "1st$ - 2nd$ = "; c.put_money();
+	/*dollars a, b, c;
+	long double time_oneitem, item_counter;
+	cout << "1: "; a.get_dollars();
+	cout << "2: "; b.get_dollars();
+
+	c = a + b; cout << "1st$ + 2nd$ = "; c.put_dollars();
+	c = a - b; cout << "1st$ - 2nd$ = "; c.put_dollars();
 	cout << "Time for one item: "; cin >> time_oneitem;
-	c = a * time_oneitem; cout << "Total price for one item per entered time: "; c.put_money();
+	c = a * time_oneitem; cout << "Total price for one item per entered time: "; c.put_dollars();
 	cout << "Count of products: "; cin >> item_counter;
-	c = a / item_counter; cout << "Price for one item: "; c.put_money();
-	item_counter = a / c; cout << "Count of products: " << item_counter;
+	c = a / item_counter; cout << "Price for one item: "; c.put_dollars();
+	item_counter = a / c; cout << "Count of products: " << item_counter;*/
 	
 	return 0;
 }
 
-money money::operator+(money m2) const {
+sterling sterling::operator+(sterling s2) const {
+	return sterling(double(sterling(pounds, shillings, pence)) + double(s2));
+}
+
+sterling sterling::operator-(sterling s2) const {
+	return sterling(double(sterling(pounds, shillings, pence)) - double(s2));
+}
+
+sterling sterling::operator*(double a) const {
+	return sterling(double(sterling(pounds, shillings, pence)) * a);
+}
+
+sterling sterling::operator/(double a) const {
+	return sterling(double(sterling(pounds, shillings, pence)) / a);
+}
+
+double sterling::operator/(sterling s2) const {
+	double a = double(sterling(pounds, shillings, pence)) / double(s2);
+	return a;
+}
+
+sterling::operator double() {
+	double s = pounds + static_cast<double>(shillings) / 20 + static_cast<double>(pence) / 240;
+	double s_fixed = round(s * 100) / 100;
+	return s_fixed;
+}
+
+sterling::operator dollars() {
+	long double dlrs = double(sterling(pounds, shillings, pence));
+	return dollars(dlrs);
+}
+
+dollars dollars::operator+(dollars m2) const {
 	long double m = value + m2.value;
-	return money(m);
+	return dollars(m);
 }
 
-money money::operator-(money m2) const {
+dollars dollars::operator-(dollars m2) const {
 	long double m = value - m2.value;
-	return money(m);
+	return dollars(m);
 }
 
-money money::operator*(long double t) const {
+dollars dollars::operator*(long double t) const {
 	long double m = value * t;
-	return money(m);
+	return dollars(m);
 }
 
-money money::operator/(long double c) const {
+dollars dollars::operator/(long double c) const {
 	long double m = value / c;
-	return money(m);
+	return dollars(m);
 }
 
-long double money::operator/(money m2) const {
+long double dollars::operator/(dollars m2) const {
 	long double c = value / m2.value;
 	return c;
 }
 
-void money::ldtoms(long double m) {
+void dollars::ldtoms(long double m) {
 	if (m > 9999999999999990.00) exit(1);
 
 	ostringstream mstr;
@@ -124,7 +207,7 @@ void money::ldtoms(long double m) {
 	cout << ms << endl;
 }
 
-long double money::mstold(char m[]) {
+long double dollars::mstold(char m[]) {
 	char mvd[19];
 	int j = 0;
 	for (int i = 0; i < 18; i++) {
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-Class Money
+Money
 ---
-In this program, money values are requested from the user and then convert from an *array of characters* to a *long double* type. You can perform any standart arithmetic operations on them using overloaded operations defined in the *Money* class. 
+In this program, we have two classes which present us two currencies - dollars and old format's sterlings.
+
+In **Dollars** class money values are requested from the user and then convert from an *array of characters* to a *long double* type. You can perform any standart arithmetic operations on them using overloaded operations defined in the class.<br> 
 If you want to display them, the special method converse money values back from *long double* to *character array*.
+
+In **Sterlings** class money values are requested from the user in old writing system, where:<br>
+1 pound = 20 shellings<br>
+1 shelling = 12 pence<br>
+You can perform any standart arithmetic operations on them using overloaded operations defined in the class. Also you can convert sterling's value to a decimal format which is a new system. 
+
+Finally, you can transform classes values to each other - sterlings to dollars and back.
